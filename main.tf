8a5ad41d3cc753e4a1329cd9f9e4f2062480a4b3
Kausik Porey <76431110+kausikporey@users.noreply.github.com>
2023-04-26 17:13:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ resource "aws_lb" "frontend"{}
 
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.frontend.arn
-  port              = "443"
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type = "redirect"
 
-    # redirect {
-    #   port        = "443"
-    #   protocol    = "HTTPS"
-    #   status_code = "HTTP_301"
-    # }
+    redirect {
+      port        = "80"
+      protocol    = "HTTP"
+      status_code = "HTTP_301"
+    }
   }
 }
