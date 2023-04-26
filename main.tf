727cfed20b1a1260cb3b1d02d600410b285fb585
Kausik Porey <76431110+kausikporey@users.noreply.github.com>
2023-04-26 18:02:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,35 @@ provider "aws" {
 
 resource "aws_lb" "frontend"{}
 
+resource "aws_lb_target_group" "ip-example" {
+  name        = "tf-example-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.aws_vpc.selected.id
+}
+
+data "aws_vpc" "selected" {
+    id = "vpc-084b927968551367f"
+}
+
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.frontend.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
+   type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
   
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ip_example.arn
   }
 }
