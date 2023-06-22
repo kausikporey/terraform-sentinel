31aafb5d4968d11ae625153c39d4959b472d6d19
Kausik Porey <kausikporey@yahoo.com>
2023-06-22 17:40:51 +0530

    first commit

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ provider "aws" {
   shared_credentials_files = ["/users/DT495QF/.aws/credentials"]
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "lb_logs" {
   bucket = "my-tf-test-bucket"
 }
 
 
-resource "aws_lb" "lb_logs" {
+resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
