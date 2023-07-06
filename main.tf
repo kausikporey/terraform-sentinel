4d1d0a582465163b18aa26d250a087402ba9f9fc
Kausik Porey <kausikporey@yahoo.com>
2023-07-06 15:30:20 +0530

    abc

--- main.tf
+++ main.tf
@@ -1,54 +1,23 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+resource "aws_kms_key" "example" {
+  description = "example"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+resource "aws_kms_key_policy" "example" {
+  key_id = aws_kms_key.example.id
+  policy = jsonencode({
+    Id = "example"
+    Statement = [
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
 }
-
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
-  runtime = "nodejs14.x"
-}
-
-resource "aws_secretsmanager_secret" "example" {
-  name = "example"
-  rotation_lambda_arn = aws_lambda_function.test_lambda.arn
-  rotation_rules {
-    automatically_after_days = 7
-  }
-}
-
-
-resource "aws_secretsmanager_secret_rotation" "example" {
-  secret_id           = aws_secretsmanager_secret.example.id
-  rotation_lambda_arn = aws_lambda_function.test_lambda.arn
-
-  rotation_rules {
-    automatically_after_days = 30
-    schedule_expression = "rate(3 days)"
-  }
-}
-
-# resource "aws_secretsmanager_secret_rotation" "example2" {
-#   secret_id           = aws_secretsmanager_secret.example.id
-#   rotation_lambda_arn = aws_lambda_function.test_lambda.arn
-
-#   rotation_rules {
-#     automatically_after_days = 60
-#     # schedule_expression = "rate(3 months)"
-#   }
-# }
