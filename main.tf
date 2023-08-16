78b420d738e27de4b821a0b23cabe36c25bbb10c
Kausik Porey <kausikporey@yahoo.com>
2023-08-16 14:48:12 +0530

    second commit

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_neptune_cluster" "neptune-cluster" {
   cluster_identifier                  = "neptune-cluster-demo"
   engine                              = "neptune"
   backup_retention_period             = 5
-  enable_cloudwatch_logs_exports      = "audit"
+  enable_cloudwatch_logs_exports      = ["audit"]
 }
